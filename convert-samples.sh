b6e362938739656038a660d8371d64092183f1df
fxprunayre <fx.prunayre@gmail.com>
2016-09-23 18:09:59 +0200

    Improve conversion.

--- convert-samples.sh
+++ convert-samples.sh
@@ -14,6 +14,9 @@ l=(
     'inspire/LC/lcvLandCoverDataset.gml::LC'
     'inspire/LC/lcvLandCoverUnit.gml::LC'
     'inspire/PS/cddaDesignatedArea.gml::PS'
+    'inspire/national/NL/cadastralparcel.gml::NL'
+    'inspire/national/NL/building.gml::NL'
+    'inspire/national/NL/cadastralboundary.gml::NL'
 )
 
 for index in "${l[@]}" ; do
@@ -21,11 +24,11 @@ for index in "${l[@]}" ; do
     VALUE="${index##*::}"
   echo "  $KEY > $VALUE ..."
   ogr2ogr db/$VALUE.sqlite GMLAS:$KEY \
-      -f sqlite -skipfailures -dsco spatialite=yes \
+      -f sqlite -append -skipfailures -nlt CONVERT_TO_LINEAR -oo swap_coordinates=no -dsco spatialite=yes \
       -oo EXPOSE_METADATA_LAYERS=YES
   ogr2ogr PG:'host=localhost user=qgis password=qgis dbname=inspire' \
       GMLAS:$KEY \
-      -f PostgreSQL -skipfailures \
+      -f PostgreSQL -skipfailures -nlt CONVERT_TO_LINEAR \
       -lco OVERWRITE=YES -lco SCHEMA=$VALUE -oo EXPOSE_METADATA_LAYERS=YES
 
 done
